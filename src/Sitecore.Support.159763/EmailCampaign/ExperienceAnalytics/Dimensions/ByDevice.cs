03142950482eaa16f23b65d7b56f5d60539dc76a
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-05-12 10:37:30 +0800

    Create Patch

---
+++ src/Sitecore.Support.159763/EmailCampaign/ExperienceAnalytics/Dimensions/ByDevice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Sitecore.Analytics.Aggregation.Data.Model;
+using Sitecore.Analytics.Model;
+using Sitecore.CES.DeviceDetection;
+using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.ExperienceAnalytics.Dimensions;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.ExperienceAnalytics.Aggregation.Data.Model;
+using Sitecore.ExperienceAnalytics.Aggregation.Dimensions;
+
+namespace Sitecore.Support.EmailCampaign.ExperienceAnalytics.Dimensions
+{
+
+  internal class ByDevice : DimensionBase
+  {
+    public ByDevice(Guid dimensionId) : base(dimensionId) { }
+    public override IEnumerable<DimensionData> GetData(IVisitAggregationContext context)
+    {
+      if (DeviceDetectionManager.IsEnabled)
+        Log.Warn("Sitecore.support.159763: Device Detection is enabled, please remove the patch by disabling the Sitecore.Support.159763.config file.", this);
+      return new List<DimensionData>();
+    }
+  }
+}
---
+++ src/Sitecore.Support.159763/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.159763")]
+[assembly: AssemblyProduct("Sitecore.Support.159763")]
+[assembly: ComVisible(false)]
